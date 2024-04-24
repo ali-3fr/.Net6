2f28563c45f729bacf316f1a193b3e88ffb1b671
ali safari <ali.3fr@gmail.com>
2024-04-24 17:52:29 +0330

    middleWares

---
+++ Platform/Middleware.cs
@@ -0,0 +1,34 @@
+﻿namespace Platform
+{
+    public class QueryStringMiddleWare
+    {
+        private RequestDelegate? next;
+
+        public QueryStringMiddleWare()
+        {
+
+        }
+
+        public QueryStringMiddleWare(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+            {
+                if (!context.Response.HasStarted)
+                {
+                    context.Response.ContentType = "text/plain";
+
+                }
+                await context.Response.WriteAsync("class-Based middleWare \n");
+            }
+            if(next != null)
+            {
+                await next(context);
+            }
+        }
+    }
+}
--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,6 +1,46 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async(context, next) =>
+{
+    await next();
+    await context.Response.WriteAsync($"\n Status Code : {context.Response.StatusCode}");
+
+
+});
+
+((IApplicationBuilder)app).Map("/branch", branch =>
+{
+    branch.UseMiddleware<Platform.QueryStringMiddleWare>();
+    branch.Run(async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("Branch MiddleWare");
+    });
+});
+//app.Use(async(context, next) =>
+//{
+//    if(context.Request.Path == "/short")
+//    {
+//        await context.Response.WriteAsync("short circuted");
+//    }
+//    else
+//    {
+//        await next();
+//    }
+//});
+
+//app.Use((async(context, next) =>
+//{
+//    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+//    {
+//        context.Response.ContentType = "text/plain";
+//        await context.Response.WriteAsync("Custome MiddleWare \n");
+//    }
+//    await next();
+//}));
+
+app.UseMiddleware<Platform.QueryStringMiddleWare>();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
