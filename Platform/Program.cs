ec3ee234a67cf87ccf5590726bbcf53dc524547c
ali safari <ali.3fr@gmail.com>
2024-04-27 17:20:17 +0330

    middlewares

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,46 +1,15 @@
+using Microsoft.Extensions.Options;
+using Platform;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<MessageOptions>(options => {
+    options.CityName = "Albany";
+});
+
+
 var app = builder.Build();
-
-app.Use(async(context, next) =>
-{
-    await next();
-    await context.Response.WriteAsync($"\n Status Code : {context.Response.StatusCode}");
-
-
-});
-
-((IApplicationBuilder)app).Map("/branch", branch =>
-{
-    branch.UseMiddleware<Platform.QueryStringMiddleWare>();
-    branch.Run(async (HttpContext context) =>
-    {
-        await context.Response.WriteAsync("Branch MiddleWare");
-    });
-});
-//app.Use(async(context, next) =>
-//{
-//    if(context.Request.Path == "/short")
-//    {
-//        await context.Response.WriteAsync("short circuted");
-//    }
-//    else
-//    {
-//        await next();
-//    }
-//});
-
-//app.Use((async(context, next) =>
-//{
-//    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
-//    {
-//        context.Response.ContentType = "text/plain";
-//        await context.Response.WriteAsync("Custome MiddleWare \n");
-//    }
-//    await next();
-//}));
-
-app.UseMiddleware<Platform.QueryStringMiddleWare>();
+app.UseMiddleware<LocationMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
