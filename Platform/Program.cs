fcaec9c7b6351995fefabd79bbcb96c781bbeb59
ali safari <ali.3fr@gmail.com>
2024-06-10 17:21:27 +0330

    job

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,22 +1,35 @@
 using Platform;
+using Platform.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSingleton<IResponseFormatter, TextResponseFormatter>();
 var app = builder.Build();
 
 
-app.MapGet("{first}/{second}/{third}", async context =>
-{
-    await context.Response.WriteAsync("Request was routed \n");
-    foreach(var kvp in context.Request.RouteValues)
-    {
-        await context.Response.WriteAsync($"{kvp.Key} : {kvp.Value}");
+app.UseMiddleware<WeatherMiddlware>();
 
-    }
+//IResponseFormatter formatter = new TextResponseFormatter();
+
+app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter formatter) =>
+{
+    await formatter.Format(context, "MiddleWareFunctrion it is snowing in chigaco");
+ 
+});;
+
+app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
+
+app.MapGet("endpoint/function", async (HttpContext context, IResponseFormatter formatter) =>
+{
+    await formatter.Format(context, "Endpoint Function: It is sunny in LA");
+
 });
 
-
-app.MapGet("capital/{country}", Capital.Endpoint);
-app.MapGet("population/{city}",Population.Endpoint)
-    .WithMetadata(new RouteNameMetadata("population"));
+//app.MapGet("capital/{country}", Capital.Endpoint);
+//app.MapGet("population/{city}",Population.Endpoint)
+//    .WithMetadata(new RouteNameMetadata("population"));
 
 app.MapFallback(async context => {
     await context.Response.WriteAsync("Routed to fallback endpoint");
---
+++ Platform/WeatherMiddlware.cs
@@ -0,0 +1,26 @@
+﻿using Platform.Services;
+
+namespace Platform
+{
+    public class WeatherMiddlware
+    {
+        private RequestDelegate next;
+        private IResponseFormatter formatter;
+        public WeatherMiddlware(RequestDelegate next, IResponseFormatter _formatter)
+        {
+            formatter = _formatter;
+            this.next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.Request.Path == "/middleware/class")
+            {
+                await formatter.Format(context, "Middleware Class: It is raining in London");
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
