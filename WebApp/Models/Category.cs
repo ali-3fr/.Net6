1d0dd7951d206f98b223fe6475fc56692a47d6fc
ali safari <ali.3fr@gmail.com>
2024-09-19 14:31:08 +0330

    18

--- WebApp/Models/Category.cs
+++ WebApp/Models/Category.cs
@@ -3,7 +3,7 @@
     public class Category
     {
         public long CategoryId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; }=String.Empty;
         public IEnumerable<Product>? Products { get; set; }
     }
 }
